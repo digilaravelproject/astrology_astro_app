f2a94f16380d26bfb2ab3f657d2e0fcf3962e2cb
Firoz Mohammad <firozm613@gmail.com>
2026-07-20 20:34:53 +0530

    fix: change Get.until to Get.back in astrologer call end button to prevent closing incoming chat request

--- lib/features/call/presentation/pages/call_screen.dart
+++ lib/features/call/presentation/pages/call_screen.dart
@@ -170,7 +170,7 @@ class _CallScreenState extends State<CallScreen> {
                         // End Call (Red Button)
                         _buildEndCallButton(onPressed: () {
                           controller.endCall();
-                          Get.until((route) => route.isFirst);
+                          Get.back();
                         }),
 
                         // Speaker button
