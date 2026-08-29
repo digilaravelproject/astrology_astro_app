c07459975a02ad08532a9ad635a1db0f001aedde
Firoz Mohammad <firozm613@gmail.com>
2026-08-29 17:18:32 +0530

    Fix: Resolve WebSocket connection failing to initialize on app start

--- lib/core/services/network/websocket_service.dart
+++ lib/core/services/network/websocket_service.dart
@@ -110,6 +110,7 @@ class WebSocketService extends GetxService {
         Logger.e('|🔌 WEBSOCKET ERROR');
         Logger.e('|⚠️ Cannot connect, token or userId is missing.');
         Logger.e('━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━');
+        _isConnecting = false;
         return;
       }
 
